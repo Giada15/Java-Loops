fc923bfc25149af9c88385da5ede02966a5ecd11
Giada15 <tascheriogiada@gmail.com>
2024-04-17 12:26:01 -0400

    Created a counter application that prompts the user to enter a number and counts until that number using a for loop

---
+++ CountingTool.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class CountingTool {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Hi Timmy! Choose a number to count to: ");
+
+        int userNumber= scan.nextInt();
+
+        System.out.println("Great! Here's how it's done");
+        for(int i=0; i<=userNumber; i++){
+            System.out.print(i + " ");
+        }
+
+        scan.close();
+
+        
+    }
+    
+}
